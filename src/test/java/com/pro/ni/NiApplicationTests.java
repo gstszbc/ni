2c53836b476ed25add945623fa59bf9464923800
v_biczhang <v_biczhang@tencent.com>
2021-12-08 09:45:55 +0800

    Initial commit

---
+++ src/test/java/com/pro/ni/NiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.ni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
